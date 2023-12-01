5df72f842ae225bc78c516c4a5deffa908fedfd4
Ty Daniels <tydaniels@Tys-MacBook-Pro.lan>
2023-11-30 22:03:39 -0600

    Moves follow cam code

--- Assets/Scripts/PlayerObject.cs
+++ Assets/Scripts/PlayerObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using TMPro;
+using StarterAssets;
 using FirstDayIn.Network;
 
 public class PlayerObject : NetworkBehaviour {
@@ -25,18 +26,23 @@ public class PlayerObject : NetworkBehaviour {
 		Index = index;
 	}
 
+    public void Awake() {
+        GetComponent<Animator>().enabled = true;
+        GetComponent<CharacterController>().enabled = true;
+        GetComponent<ThirdPersonController>().enabled = true;
+    }
+
+    public void Start() {
+        PlayerName = PlayerPrefs.GetString("Username");
+        Local = this;
+    }
+
     public override void Spawned() {
         base.Spawned();
 
         Debug.Log("PlayerObject Spawned()");
 
-        if (Runner.LocalPlayer == Object.InputAuthority) {
-            Debug.Log("PlayerObject HasInputAuthority");
-            PlayerName = PlayerPrefs.GetString("Username");
-            Local = this;
-        }
-
-         if (Runner.IsServer) {
+        if (Runner.IsServer) {
             PlayerRegistry.Server_Add(Runner, Object.InputAuthority, this);
         }
     }
